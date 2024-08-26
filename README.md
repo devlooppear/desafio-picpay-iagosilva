9e4b3573c00d1d54bedad98cd770ea16f90547e3
devlooppear <iago.profissional.developer@gmail.com>
2024-08-26 11:40:01 -0300

    feat: configuring migrations

---
+++ README.md
@@ -0,0 +1,23 @@
+# Get Started
+
+Para iniciar o projeto, siga as etapas abaixo:
+
+1. **Limpar e Instalar Dependências**:
+
+   Execute o seguinte comando para limpar e instalar todas as dependências do projeto:
+
+```bash
+mvn clean install
+```
+
+2. **Executar a Aplicação**: Para iniciar a aplicação Spring Boot, use o comando:
+
+```bash
+./mvnw spring-boot:run
+```
+
+3. **Executar Migrações do Flyway**: Para executar as migrações do Flyway, utilize o seguinte comando, certificando-se de que o arquivo de configuração esteja no caminho correto:
+
+```bash
+mvn -Dflyway.configFiles=src/main/resources/flyway.conf flyway:migrate
+```
---
+++ src/main/resources/db/migration/V1__create_users_and_transactions_tables.sql
@@ -0,0 +1,21 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255) NOT NULL,
+    cpf VARCHAR(11) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    user_type VARCHAR(50) NOT NULL,
+    balance DECIMAL(10, 2) DEFAULT 0
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    amount DECIMAL(10, 2) NOT NULL,
+    payer_id INT NOT NULL,
+    payee_id INT NOT NULL,
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (payer_id) REFERENCES users(id),
+    FOREIGN KEY (payee_id) REFERENCES users(id)
+);
