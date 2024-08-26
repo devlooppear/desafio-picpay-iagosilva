1e11a6a191198fafdbe3b431cb143f8729677a2e
devlooppear <iago.profissional.developer@gmail.com>
2024-08-26 09:26:29 -0300

    first commit

---
+++ src/main/java/com/example/desafio_picpay_iago_silva_devlooppear/DesafioPicpayIagoSilvaDevlooppearApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_picpay_iago_silva_devlooppear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPicpayIagoSilvaDevlooppearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPicpayIagoSilvaDevlooppearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafio_picpay_iago_silva_devlooppear/DesafioPicpayIagoSilvaDevlooppearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_picpay_iago_silva_devlooppear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPicpayIagoSilvaDevlooppearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
